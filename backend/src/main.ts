b689faab77dc80caa7a73bf33cc303353c8e775b
hagaradel12 <hagaradel198@gmail.com>
2024-11-30 20:07:58 +0200

    auth part 1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,11 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow requests from Next.js server
+    methods:'GET,POST,PUT,PATCH,DELETE',
+    credentials: true,
+  });
   dotenv.config();
   await app.listen(3001);
 }
